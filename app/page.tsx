81041c365cb432c4e89c2257de9ecd884fd54366
khadijaamjad <khadijah.amjad25@gmail.com>
2025-01-28 18:43:50 +0500

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl">Welcome</h1>
+      <Link href="/properties">Show Properties</Link>
+    </div>
+  );
+}
---
+++ app/properties/add/page.tsx
@@ -0,0 +1,5 @@
+const PropertyAddPage = () => {
+  return <div>Property AddPage</div>;
+};
+
+export default PropertyAddPage;
